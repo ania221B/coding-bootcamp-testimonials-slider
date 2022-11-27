eb6f5904cbee593f0f6d2c3d386f8c11e816a583
ania221B <abuwaj@gmail.com>
2022-11-27 22:27:48 +0100

    js refactor

--- js/script.js
+++ js/script.js
@@ -3,61 +3,32 @@ const imagesContainer = carousel.querySelector('.carousel__image-list')
 const contentContainer = carousel.querySelector('.carousel__article-list')
 const images = [...imagesContainer.querySelectorAll('.slide')]
 const contents = [...contentContainer.querySelectorAll('.slide')]
-const previousButton = carousel.querySelector('.jsBtnPrev')
+// const previousButton = carousel.querySelector('.jsBtnPrev')
 const nextButton = carousel.querySelector('.jsBtnNext')
 
-nextButton.addEventListener('click', e => {
-  if (!e.target.closest('.btn')) return
+function switchSlide (array, button) {
+  const width = array[0].getBoundingClientRect().width
+  const parent = array.find(item => item.classList.contains('is-selected')).parentElement
+  const currentIndex = array.findIndex(item => item.classList.contains('is-selected'))
+  const targetIndex = button === nextButton
+    ? currentIndex + 1
+    : currentIndex - 1
 
-  const imageWidth = images[0].getBoundingClientRect().width
-  const currentIndex = images.findIndex(image => image.classList.contains('is-selected'))
-  const targetIndex = currentIndex + 1
-  const targetImage = images[targetIndex]
-  const amountToMove = imageWidth * targetIndex + 'px'
+  const target = array[targetIndex]
+  const amountToMove = width * targetIndex + 'px'
 
-  images.forEach(img => img.classList.remove('is-selected'))
+  array.forEach(arrayItem => arrayItem.classList.remove('is-selected'))
 
-  targetImage.classList.add('is-selected')
-  imagesContainer.style.transform = `translateX(-${amountToMove})`
-  imagesContainer.style.transition = 'transform 500ms ease-in-out'
+  target.classList.add('is-selected')
+  parent.style.transform = `translateX(-${amountToMove})`
+  parent.style.transition = 'transform 500ms ease-in-out'
+}
 
-  const contentWidth = contents[0].getBoundingClientRect().width
-  const currentContentIndex = contents.findIndex(content => content.classList.contains('is-selected'))
-  const targetContentIndex = currentContentIndex + 1
-  const targetContent = contents[targetContentIndex]
-  const amountToMove2 = contentWidth * targetContentIndex + 'px'
+carousel.addEventListener('click', e => {
+  const clickedButton = e.target.closest('.btn')
 
-  contents.forEach(text => text.classList.remove('is-selected'))
+  if (!e.target.closest('.jsCarouselBtn')) return
 
-  targetContent.classList.add('is-selected')
-  contentContainer.style.transform = `translateX(-${amountToMove2})`
-  contentContainer.style.transition = 'transform 500ms ease-in-out'
-})
-
-previousButton.addEventListener('click', e => {
-  if (!e.target.closest('.btn')) return
-
-  const imageWidth = images[0].getBoundingClientRect().width
-  const currentIndex = images.findIndex(image => image.classList.contains('is-selected'))
-  const targetIndex = currentIndex - 1
-  const targetImage = images[targetIndex]
-  const amountToMove = imageWidth * targetIndex + 'px'
-
-  images.forEach(img => img.classList.remove('is-selected'))
-
-  targetImage.classList.add('is-selected')
-  imagesContainer.style.transform = `translateX(-${amountToMove})`
-  imagesContainer.style.transition = 'transform 500ms ease-in-out'
-
-  const contentWidth = contents[0].getBoundingClientRect().width
-  const currentContentIndex = contents.findIndex(content => content.classList.contains('is-selected'))
-  const targetContentIndex = currentContentIndex - 1
-  const targetContent = contents[targetContentIndex]
-  const amountToMove2 = contentWidth * targetContentIndex + 'px'
-
-  contents.forEach(text => text.classList.remove('is-selected'))
-
-  targetContent.classList.add('is-selected')
-  contentContainer.style.transform = `translateX(-${amountToMove2})`
-  contentContainer.style.transition = 'transform 500ms ease-in-out'
+  switchSlide(images, clickedButton)
+  switchSlide(contents, clickedButton)
 })
